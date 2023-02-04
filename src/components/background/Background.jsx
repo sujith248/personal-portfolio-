5c5e642756dcf88df65ce16c974abd70ee348512
sujith248 <52060798+sujith248@users.noreply.github.com>
2023-02-04 18:32:39 +1100

    Update Background.jsx

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -7,19 +7,19 @@ const Background = () => {
             <br />
             <p>
                 I am an aspiring final-year software engineering student, a part-time software intern at UTS Rapido, and a coding instructor.
-                <p>
+                <br />
                 I have a strong passion for coding and am proficient in a variety of programming languages, database frameworks, and other software frameworks. 
-                </p>
                 I would love to use these skills to contribute to the creation of useful software products. 
-                <p>Being a full-stack web developer is my career objective, and now I’m on the journey of achieving my goal. </p>
-                </p>
-                <p>
+                <br />
+                Being a full-stack web developer is my career objective, and now I’m on the journey of achieving my goal.
+                <br />
                 During my time at university, I have gained various skills in software and out of all the other streams in software, web development was the one that caught my interest and that’s when I decided to pursue it as my career. 
                 The work experience I have gained so far has taught me the importance of engineering and agile development along with the impact it makes on your career.
-                <p>I have built a few projects such as a model restaurant website, and a model banking website during my free time.</p>
-                <p>
+                <br />
+                I have built a few projects such as a model restaurant website, and a model banking website during my free time.
+                <br />
+               
                 I'm still driven and diligent, and I try to take advantage of any chance I have. In the coming years, I hope to learn and grow with every opportunity. 
-                </p>
             </p>
         </div>
     )
